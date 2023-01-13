d0f8d8fbda93a5f3d31c47e34124e8c9fe1e2da2
Azrul Haris <azrulharis85@gmail.com>
2023-01-13 12:07:48 +0800

    Update depedency

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function Navigation(props) {
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          React
+          Home
         </Link>
         <ul className="navbar-nav me-auto">
           <li className="nav-item">
